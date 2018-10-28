d0946158b0ec068b74e11ddd4d32120e59df6c6a
Stanislav Iablokov <reg@iablokov.ru>
2018-10-28 21:54:24 +0300

    28.10

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@
 using namespace std;
 int main()
 {
-    double r0[3] = {0.0, 0.0, 0.0};
-    double v0[3] = {0.0, 0.0, 0.0};
 
     double radius = 1.0;
 
@@ -22,9 +20,6 @@ int main()
     for(int i = 0; i < 1; i++)
     {
         Particle particle = Particle(radius);
-        //particle.x = 0;  particle.y = 0;  particle.z = 0;
-        //particle.vx = 0; particle.vy = 0; particle.vz = 0;
-
         system.addParticle(particle, true);   // true for random position
     }
 
